9884e87aa04a248eb7abb71a05e79057dd78cef1
pichet <good100vichet@gmail.com>
2025-09-07 23:53:06 +0700

    Update

--- api-getway/src/main/java/com/example/apigetway/config/GatewayRoutesConfig.java
+++ api-getway/src/main/java/com/example/apigetway/config/GatewayRoutesConfig.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class GatewayRoutesConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service-route", r -> r
